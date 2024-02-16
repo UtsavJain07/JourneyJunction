ae7bdb2093497e531f501cab47f2b652933695ba
Utsav Jain <99479010+UtsavJain07@users.noreply.github.com>
2024-02-16 22:18:10 +0530

    Add files via upload

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ import mysql.connector
 mydb=mysql.connector.connect(host='localhost',user='root',password='Utsav#Jain07',database='railwaydb')
 
 cur=mydb.cursor()
-print(mydb.connection_id)
+# print(mydb.connection_id)
 
 source=input("Enter Source: ")
 destination=input("Enter destination: ")
@@ -21,25 +21,16 @@ WHERE b.station_name=%s AND a.station_name=%s AND (a.distance-b.distance)>0;
 
 header=['Train_no','Train_Name','Source Station Name','Destination Station Name','Total Distance', 
 'Fare','Time_Taken']
+
+
 data=(source,destination)
 cur.execute(s2,data)
 result=cur.fetchall()
-# c=1
-# for row in result:
-#     print(f"\n*****Result {c}*****\n")
-#     for index,details in enumerate(row):
-#         print(f"{header[index]} : {details}")
-#         # if(header[index]=='Time_Taken'):
-#         #     if(int(details)<0):
-#         #         details=240000-int(details)
-#     c+=1
+c=1
+for row in result:
+    print(f"\n*****Result {c}*****\n")
+    for index,details in enumerate(row):
+        print(f"{header[index]} : {details}")
+    c+=1
 
-for r in result:
-    print(r)
-    
-
-
-
-# total_distance=0
-# time=0
-
+print("\n\nHappy To Serve You!!")
